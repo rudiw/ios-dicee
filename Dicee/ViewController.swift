bb24b59030638536cd90c1e0f945b13633390ee4
Rudi Wijaya <rudiwijaya@rudis-Mac-mini.local>
2019-02-18 14:24:01 +0700

    connect component to code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func doRoll(_ sender: UIButton) {
+        
+    }
+    
 }
 

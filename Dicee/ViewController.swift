965bb3d10e7950e0c9e1e40193c64a387654900a
rudiwijaya <rudi.wijaya@mitrais.com>
2019-02-19 09:08:14 +0700

    motion ended

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,25 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var dice1: UIImageView!
     @IBOutlet weak var dice2: UIImageView!
     
-    let diceImages : [String] = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    let diceImages : [String] = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"];
     
-    var valueDice1: Int = 0
-    var valueDice2: Int = 0
+    var valueDice1: Int = 0;
+    var valueDice2: Int = 0;
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        randomAndUpdateDices()
+        randomAndUpdateDices();
     }
 
     @IBAction func doRoll(_ sender: UIButton) {
-        randomAndUpdateDices()
+        randomAndUpdateDices();
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == UIEvent.EventSubtype.motionShake {
+            randomAndUpdateDices();
+        }
     }
     
     func randomAndUpdateDices() {
-        valueDice1 = Int.random(in: 0...5)
-        valueDice2 = Int.random(in: 0...5)
+        valueDice1 = Int.random(in: 0...5);
+        valueDice2 = Int.random(in: 0...5);
         
         self.dice1.image = UIImage(named: diceImages[valueDice1]);
         self.dice2.image = UIImage(named: diceImages[valueDice2]);

9c907ae924eaa774aad29a4ad999699f27719a36
rudiwijaya <rudi.wijaya@mitrais.com>
2019-02-19 08:31:11 +0700

    use array

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,23 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var dice1: UIImageView!
     @IBOutlet weak var dice2: UIImageView!
     
-    var valueDice1: Int = 6
-    var valueDice2: Int = 6
+    let diceImages : [String] = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var valueDice1: Int = 0
+    var valueDice2: Int = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.dice1.image = UIImage(named: ("dice\(valueDice1)"));
-        self.dice2.image = UIImage(named: ("dice\(valueDice2)"));
+        self.dice1.image = UIImage(named: diceImages[valueDice1]);
+        self.dice2.image = UIImage(named: diceImages[valueDice2]);
     }
 
     @IBAction func doRoll(_ sender: UIButton) {
-        valueDice1 = Int.random(in: 1...6)
-        valueDice2 = Int.random(in: 1...6)
+        valueDice1 = Int.random(in: 0...5)
+        valueDice2 = Int.random(in: 0...5)
         
-        self.dice1.image = UIImage(named: ("dice\(valueDice1)"));
-        self.dice2.image = UIImage(named: ("dice\(valueDice2)"));
+        self.dice1.image = UIImage(named: diceImages[valueDice1]);
+        self.dice2.image = UIImage(named: diceImages[valueDice2]);
        
     }
     
